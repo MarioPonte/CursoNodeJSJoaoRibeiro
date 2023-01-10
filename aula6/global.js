2331b26169ff3c8a6ef4aa5da6406ccd3c839be0
Mário Ponte <ponteolavo30@gmail.com>
2023-01-10 22:36:11 +0000

    aula6

---
+++ aula6/global.js
@@ -0,0 +1,16 @@
+/* Global object test
+console.log("OBJETO GLOBAL");
+console.log(global);
+*/
+
+/* setTimeout test
+console.clear();
+console.log("TEST:");
+
+setTimeout(() => {
+    console.log("Done!");
+}, 3000);
+*/
+
+console.log(__dirname);
+console.log(__filename);
