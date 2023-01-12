d5bda528ebdd84f2ec505cb37c33f4eb2dbf0043
Mário Ponte <ponteolavo30@gmail.com>
2023-01-12 23:16:05 +0000

    edit aula8

--- aula8/main.js
+++ aula8/main.js
@@ -5,4 +5,4 @@ console.log("Inicio");
 const { nomes, cidades, texto, adicionar } = require("./modulo");
 console.log(nomes[1] + " - " + cidades[1]);
 console.log(texto);
-console.log("Resultado: " + adicionar(1,1));
+console.log("Resultado da conta: " + adicionar(10,12));
