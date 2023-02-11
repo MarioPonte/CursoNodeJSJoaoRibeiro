a0a84cef24487ff181baf43543711debe46772e3
Mário Ponte <ponteolavo30@gmail.com>
2023-02-11 19:36:16 +0000

    aula26

--- express_01/app.js
+++ express_01/app.js
@@ -6,9 +6,10 @@ const app = express()
 app.listen(3000)
 
 app.get("/", (req, res) => {
-    res.send("<h1>Teste</h1>")
+    //res.send("<h1>Teste</h1>")
+    res.sendFile("./views/home.html", { root: __dirname })
 })
 
 app.get("/sobre", (req, res) => {
-    res.send("<h1>Sobre</h1>")
+    res.sendFile("./views/about.html", { root: __dirname })
 })
