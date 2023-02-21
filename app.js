54fa0d01a5ea7c829e24c55af7aa21ea36a54913
Mário Ponte <ponteolavo30@gmail.com>
2023-02-21 21:47:20 +0000

    aula30

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ app.set('view engine', 'ejs')
 app.listen(3000)
 
 app.get("/", (req, res) => {
-    res.render('home', { title:"Home" })
+    res.render('home', { title:"Home", subtitle:"My home" })
 })
 
 app.get("/services", (req, res) => {
-    res.render('services', { title:"Services" })
+
+    const services = [
+        { titulo_servico: "Desenvolvimento web", descricao: "É bom" },
+        { titulo_servico: "Desenvolvimento Desktop", descricao: "É para apps de desktop" },
+        { titulo_servico: "Desenvolvimento Mobile", descricao: "React native" },
+    ];
+
+    res.render('services', { title:"Services", services })
 })
 
 app.get("/about", (req, res) => {
